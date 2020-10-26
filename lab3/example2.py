a240eae976755b815fc00f83b6e59f98891f8146
q36762000 <enesyilmaz@std.iyte.edu.tr>
2020-10-26 08:37:50 +0000

    example2

--- lab3/example2.py
+++ lab3/example2.py
@@ -2,9 +2,9 @@ num1 = int(input("Enter a number:"))
 num2 = int(input("Enter a number:"))
 num3 = int(input("Enter a number:"))
 
-if num1 > num2 and num3:
+if num1 < num2 and num3:
   print(num1)
-elif num2 > num1 and num3:
+elif num2 < num1 and num3:
   print(num2)
 else:
   print(num3)
