d2fd3fbb32272ec10a28311b965897c64118cbd7
q36762000 <enesyilmaz@std.iyte.edu.tr>
2020-11-16 09:08:09 +0000

    example2

---
+++ lab5/example2.py
@@ -0,0 +1,12 @@
+num_of_ints = int(input("how many integers: "))
+even = 0
+odd = 0
+
+for i in range(num_of_ints):
+  x = int(input("enter an integer: "))
+  if x % 2 == 0:
+    even += 1
+  else:
+    odd += 1
+percent = even / num_of_ints * 100
+print(percent)
