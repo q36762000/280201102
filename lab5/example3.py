72842f6e14b35378b701017eb482812cf1e4899f
q36762000 <enesyilmaz@std.iyte.edu.tr>
2020-11-16 10:34:47 +0000

    example3

--- lab5/example3.py
+++ lab5/example3.py
@@ -2,9 +2,9 @@ x1 = int(input("enter an integer: "))
 x2 = int(input("enter an integer: "))
 matching_digits = 0
 
-while x1 > 0:
-  if str(x1 % 10) in list(str(x2)):
-    matching_digits += 1
-  x1 = x1 // 10
+for a in list(str(x1)):
+  for b in set(str(x2)):
+    if a == b:
+      matching_digits += 1
 
 print(matching_digits)
