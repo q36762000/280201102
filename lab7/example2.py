bdf0f790754a8372d9783c199a8773876b58e68d
q36762000 <enesyilmaz@std.iyte.edu.tr>
2020-11-30 09:14:56 +0000

    example2

--- lab7/example2.py
+++ lab7/example2.py
@@ -1,7 +1,5 @@
 book = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
-count = 0
 book_dict = {}
 for i in book:
-  len2 = len(set(i))
-  book_dict[i] = (len(i), len2)
+  book_dict[i] = (len(i), len(set(i)), (len(i)+len(set(i)))/2)
 print(book_dict)
