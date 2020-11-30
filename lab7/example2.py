3f7f877ffb4290dd0fc231c3b745a248c6312d5b
q36762000 <enesyilmaz@std.iyte.edu.tr>
2020-11-30 09:03:02 +0000

    example2

--- lab7/example2.py
+++ lab7/example2.py
@@ -1,13 +1,7 @@
 book = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
 count = 0
 book_dict = {}
-d = []
 for i in book:
-  for k in i:
-    if k not in d:
-      d.append(k)
-      count += 1
-  book_dict[i] = (len(i), count)
-  count = 0
-  d = []
+  len2 = len(set(i))
+  book_dict[i] = (len(i), len2)
 print(book_dict)
